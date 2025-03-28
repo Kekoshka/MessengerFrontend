8d4c964cf73d1ad5ca9b2582e07fa7aae70ed858
Alexandr Isakov <ks-22-1-8@permaviat.ru>
2025-03-28 19:51:57 +0500

    разработаны user controls и главная страница

--- Models/Messages.cs
+++ Models/Messages.cs
@@ -11,10 +11,7 @@ namespace MessengerWebAPIFrontend.Models
         public int Id { get; set; }
         public int UserId { get; set; }
         public string ChatId { get; set; }
-        DateTime Created { get; set; }
+        DateTime PostDate { get; set; }
         public string Message { get; set; }
-        public byte[] Image { get; set; }
-        public byte[] Audio { get; set; }
-        public byte[] Video { get; set; }
     }
 }
